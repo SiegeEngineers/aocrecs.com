91ea840b80665284c8509b6a8880c041aa0c231e
happyleavesaoc <happyleaves.tfr@gmail.com>
2020-02-20 02:35:00 +0000

    fix caching

--- python/aocrecs/logic/report.py
+++ python/aocrecs/logic/report.py
@@ -73,7 +73,7 @@ async def rankings(database, platform_id, ladder_id, year, month, limit): # pyli
 async def most_improvement(database, platform_id, ladder_id, year, month, limit): # pylint: disable=too-many-arguments
     """Get most improvement for report interval."""
     query = """
-    select max(players.user_id) as user_id, max(players.user_name) as user_name, min(players.rate_snapshot) as min_rate,
+    select max(players.user_id) as user_id, max(players.user_name) as user_name, max(players.name) as name, min(players.rate_snapshot) as min_rate,
         max(players.rate_snapshot) as max_rate, max(players.rate_snapshot) - min(players.rate_snapshot) as diff_rate,
         count(matches.id) as count,
         sum(case when players.winner is true then 1 else 0 end) as wins,
@@ -93,13 +93,13 @@ async def most_improvement(database, platform_id, ladder_id, year, month, limit)
         dict(
             r,
             rank=i + 1,
-            user=dict(id=r['user_id'], platform_id=platform_id, name=r['user_name'])
+            user=dict(id=r['user_id'], platform_id=platform_id, name=r['user_name'] if r['user_name'] else r['name'])
         ) for i, r in enumerate(results)
     ]
 
 
 @cached(ttl=None)
-async def report(database, year, month, platform_id, limit):
+async def report(database, platform_id, year, month, limit):
     """Get a report."""
     matches_query = """
         select count(*) as count
