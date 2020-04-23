4c37bec9f745799225648783d6486d4a7832e6a6
happyleavesaoc <happyleaves.tfr@gmail.com>
2020-04-23 15:58:10 +0000

    paginate players; add country select

--- js/src/Players.js
+++ js/src/Players.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {makeStyles} from '@material-ui/styles'
 
 import Card from '@material-ui/core/Card'
@@ -10,9 +10,16 @@ import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Typography from '@material-ui/core/Typography'
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import Select from '@material-ui/core/Select'
+
+import {sortBy, uniqBy} from 'lodash'
 
 import ReactCountryFlag from 'react-country-flag'
 
+import PaginatedTable from './util/PaginatedTable'
 import AppLink from './util/AppLink'
 import CardIconHeader from './util/CardIconHeader'
 import DataQuery from './util/DataQuery'
@@ -31,27 +38,31 @@ const useStyles = makeStyles({
 
 const PeopleTable = ({rows, selected}) => {
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell align='right'></TableCell>
-          <TableCell>Name</TableCell>
-          <TableCell>Activity</TableCell>
-          <TableCell align='right'>Matches</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row, index) =>
-          <TableRow key={index} selected={selected === row.id}>
-            <TableCell align='right'>{row.country && <ReactCountryFlag countryCode={row.country} title={row.country.toUpperCase()} svg />}</TableCell>
-            <TableCell><AppLink path={['players', row.id]} text={row.name} /></TableCell>
-            <TableCell>{row.first_year} - {row.last_year}</TableCell>
-            <TableCell align='right'>{row.match_count.toLocaleString()}</TableCell>
-
+    <PaginatedTable limit={25} rows={rows}>
+    {(rows) => (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align='right'></TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Activity</TableCell>
+            <TableCell align='right'>Matches</TableCell>
           </TableRow>
-        )}
-      </TableBody>
-    </Table>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, index) =>
+            <TableRow key={index} selected={selected === row.id}>
+              <TableCell align='right'>{row.country && <ReactCountryFlag countryCode={row.country} title={row.country.toUpperCase()} svg />}</TableCell>
+              <TableCell><AppLink path={['players', row.id]} text={row.name} /></TableCell>
+              <TableCell>{row.first_year} - {row.last_year}</TableCell>
+              <TableCell align='right'>{row.match_count.toLocaleString()}</TableCell>
+
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    )}
+    </PaginatedTable>
   )
 }
 
@@ -112,6 +123,7 @@ const Person = ({id}) => {
 
 
 const PlayersView = ({match}) => {
+  const [country, setCountry] = useState('all')
   return (
     <div>
       <Typography variant='h5'>Known Players</Typography>
@@ -121,10 +133,23 @@ const PlayersView = ({match}) => {
         <Grid item xs={3}>
           <DataQuery query={GetPeople}>
             {(data) => (
+              <>
+              <FormControl>
+                <InputLabel htmlFor='country'>Country</InputLabel>
+                <Select value={country} onChange={(e, v) => setCountry(e.target.value)}>
+                  <MenuItem key={0} value={'all'}>All</MenuItem>
+                  {sortBy(uniqBy(data.people, 'country'), 'country').filter(row => row.country !== null).map((person, i) =>
+                    <MenuItem key={i+1} value={person.country}>
+                      <ReactCountryFlag countryCode={person.country} title={person.country.toUpperCase()} svg />&nbsp;{person.country.toUpperCase()}
+                    </MenuItem>
+                  )}
+                </Select>
+              </FormControl>
               <PeopleTable
-                rows={data.people}
+                rows={data.people.filter(row => country !== 'all' ? row.country === country : true)}
                 selected={parseInt(match.params.id)}
-               />
+              />
+              </>
             )}
           </DataQuery>
         </Grid>
